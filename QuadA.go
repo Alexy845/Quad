f30b3653861c2e1d5075cab0ccc9b9aaae92f231
Gilliard Yanis <yanis.gilliard@ynov.com>
2022-09-10 17:21:40 +0200

    test

--- QuadA.go
+++ QuadA.go
@@ -1,1 +1,36 @@
 package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadA(x, y int) {
+	if x > 1 {
+		z01.PrintRune('o')
+	}
+	for a := 1; a < x-1; a++ {
+		z01.PrintRune('-')
+	}
+	z01.PrintRune('o')
+	z01.PrintRune('\n')
+	for b := 1; b < y-1; b++ {
+		if y > 1 {
+			z01.PrintRune('|')
+		}
+		for c := 1; c < x-1; c++ {
+			z01.PrintRune(' ')
+		}
+		if x > 1 {
+			z01.PrintRune('|')
+		}
+		z01.PrintRune('\n')
+	}
+	if x > 1 {
+		z01.PrintRune('o')
+	}
+	for a := 1; a < x-1; a++ {
+		z01.PrintRune('-')
+	}
+	if y > 1 {
+		z01.PrintRune('o')
+		z01.PrintRune('\n')
+	}
+}
---
+++ QuadB.go
@@ -0,0 +1,7 @@
+package piscine
+
+func QuadB(x,y int) {
+	if x < 1 || y < 1 {
+		return
+
+}
